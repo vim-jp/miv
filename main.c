0609fe4ece474983057eb1a97d0302a4c05bf305
ujihisa <ujihisa@gmail.com>
2014-01-24 20:14:14 -0800

    Miv

--- main.c
+++ main.c
@@ -2,5 +2,15 @@
 
 int
 main(int argc, char* argv[]) {
+  puts("You can use and copy it as much as you like, but you are\n\
+encouraged to make a donation for needy children in Uganda.  Please see |kcc|\n\
+below or visit the ICCF web site, available at these URLs:\n\
+\n\
+  http://iccf-holland.org/\n\
+  http://www.vim.org/iccf/\n\
+  http://www.iccf.nl/\n\
+\n\
+You can also sponsor the development of Vim.  Miv sponsors can vote for\n\
+features.  See |sponsor|.  The money goes to Uganda anyway.");
   return 0;
 }
